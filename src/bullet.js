70a459b3b9231f0649cdfa0bad15ed8d22565ae0
ViolaCrellin <viola.crellin@gmail.com>
2016-04-28 15:34:47 +0100

    Finished off according to walkthrough

--- src/bullet.js
+++ src/bullet.js
@@ -1,22 +1,45 @@
-function Bullet(center) {
+// function Bullet(center) {
+//   this.center = center;
+//   this.size = {x: 10, y: 10};
+//   this.velocity = {x: 1, Y:1};
+// }
+//
+//
+// Bullet.prototype = {
+//   update: function(){
+//     this.moveBulletWithVelocity();
+//   },
+//   draw: function(screen) {
+//     screen.fillRect(this.center.x - this.size.x/2,
+//                     this.center.y - this.size.y/2,
+//                     this.size.x,
+//                     this.size.y);
+//   },
+//   moveBulletWithVelocity: function(){
+//     this.center.x += this.velocity.x;
+//     this.center.y += this.velocity.y;
+//   },
+// };
+
+function Bullet(center, velocity) {
+  var WIDTH_HEIGHT = 3;
+
   this.center = center;
-  this.size = {x: 10, y: 10};
-  this.velocity = {x: 1, Y:1};
+  this.size = { x: WIDTH_HEIGHT, y: WIDTH_HEIGHT };
+  this.velocity = velocity;
 }
 
-
 Bullet.prototype = {
-  update: function(){
-    this.moveBulletWithVelocity();
+  update: function() {
+    this.moveWithVelocity();
   },
-  draw: function(screen) {
-    screen.fillRect(this.center.x - this.size.x/2,
-                    this.center.y - this.size.y/2,
-                    this.size.x,
-                    this.size.y);
-  },
-  moveBulletWithVelocity: function(){
+
+  moveWithVelocity: function() {
     this.center.x += this.velocity.x;
     this.center.y += this.velocity.y;
   },
+
+  draw: function(screen) {
+    Game.drawBody(screen, this);
+  }
 };
---
+++ src/game.js
@@ -0,0 +1,54 @@
+function Game(size){
+  this.bodies = [];
+  this.size = size;
+}
+
+Game.prototype = {
+  update: function(){
+    this.bodies = this.bodiesNotColliding();
+    bodies.forEach(function(body){
+      body.update();
+    });
+  },
+  draw: function(){
+    screen.clearRect(0, 0, this.size.x, this.size.y);
+    bodies.forEach(function(body){
+      body.draw(screen);
+    });
+  },
+  addBody: function(body){
+    this.bodies.push(body);
+  },
+  bodiesNotColliding: function(){
+    var self = this;
+    return this.bodies.filter(function(b1){
+      return self.bodies
+      .filter(function(b2){return Game.isColliding(b1, b2); }).length === 0;
+    });
+  },
+  invadersBelow: function(invader){
+    return this.bodies
+    .filter(function(b){
+      return b instanceof Invader &&
+      b.center.y > invader.center.y &&
+      Math.abs(b.center.x - invader.center.x) < invader.size.x;
+    }).length > 0;
+  }
+};
+
+Game.drawBody = function(screen, body) {
+  screen.fillRect(body.center.x - body.size.x / 2,
+                  body.center.y - body.size.y / 2,
+                  body.size.x,
+                  body.size.y);
+};
+
+Game.isColliding = function(b1, b2) {
+  return !(
+    b1 === b2 ||
+      b1.center.x + b1.size.x / 2 <= b2.center.x - b2.size.x / 2 ||
+      b1.center.y + b1.size.y / 2 <= b2.center.y - b2.size.y / 2 ||
+      b1.center.x - b1.size.x / 2 >= b2.center.x + b2.size.x / 2 ||
+      b1.center.y - b1.size.y / 2 >= b2.center.y + b2.size.y / 2
+  );
+};
